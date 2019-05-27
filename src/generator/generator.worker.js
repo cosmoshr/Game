019fcfb4d321f4f4ad4ecbff6c44422c2e5d3911
trickypr <trickypr@users.noreply.github.com>
2019-05-27 13:45:30 +1000

    Fix code factor errors

--- src/generator/generator.worker.js
+++ src/generator/generator.worker.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import 'subworkers'
 import BuildSolarSystem from './buildSolarSystem.worker'
 import generateSolarSystem from './generateSolarSystem'
