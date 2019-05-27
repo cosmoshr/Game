025ddc4ea6d17567ae75b681d3f4aa778d753ef2
codefactor-io <support@codefactor.io>
2019-05-27 04:48:03 +0000

    [CodeFactor] Apply fixes to commit fdffc9c

--- postBuild.js
+++ postBuild.js
@@ -4,8 +4,5 @@ const { version } = require('./package.json')
 
 const { platform } = process
 
-if (platform === 'darwin')
-  renameSync(`./dist/Cosmos Habititation Race-${version}.dmg`, 'Cosmos_HR_Mac.dmg')
-else if (platform === 'win32')
-  renameSync(`./dist/Cosmos Habitiation Race Setup ${version}.exe`, 'Cosmos_HR_Windows.exe')
-
+if (platform === 'darwin') renameSync(`./dist/Cosmos Habititation Race-${version}.dmg`, 'Cosmos_HR_Mac.dmg')
+else if (platform === 'win32') renameSync(`./dist/Cosmos Habitiation Race Setup ${version}.exe`, 'Cosmos_HR_Windows.exe')
