509e94a92a26e7a1617db7bede159279b6361dd1
trickypr <trickypr@users.noreply.github.com>
2019-05-31 13:25:16 +1000

    Add moons to render engine

---
+++ src/classes/moons.js
@@ -0,0 +1,22 @@
+import { Sprite, Loader } from 'pixi.js'
+
+export default class Moon extends Sprite {
+  constructor(moon) {
+    const textureName = `moon_${moon.type.name}`
+    super(Loader.shared.resources[textureName].texture)
+
+    this.p = moon.path
+    this.d = moon.degrees
+    this.size = moon.size
+  }
+
+  init() {
+    const pos = Math.genPosOnCircle(0, 0, this.p, this.d)
+
+    this.position.x = pos.x
+    this.position.y = pos.y
+
+    this.width = this.size
+    this.height = this.size
+  }
+}
--- src/loaders/textures.js
+++ src/loaders/textures.js
@@ -1,13 +1,10 @@
 import planetTypes from '../constants/planets/_types'
+import moonTypes from '../constants/moons/_types'
 
 const textures = [
   {
     name: 'sun',
     url: 'assets/img/Stars/Sun1.png'
-  },
-  {
-    name: 'planet',
-    url: 'assets/img/Planets/Rocky/0.png'
   }
 ]
 
@@ -18,11 +15,17 @@ export function init() {
   planetTypes.forEach(planetName => {
     // eslint-disable-next-line import/no-dynamic-require, global-require
     const planet = require(`../constants/planets/${planetName}.js`).default
-    for (let i = 0; i < planet.numberOfTextures; i++) textures[textures.length] = {
+    for (let i = 0; i < planet.numberOfTextures; i++) textures.push({
       name: `Planets_${planetName}_${i}`,
       url: `assets/img/Planets/${planetName}/${i}.png`
-    }
+    })
   })
+
+  moonTypes.forEach(planetName =>
+    textures.push({
+      name: `moon_${planetName}`,
+      url: `assets/img/Moons/${planetName}.png`
+    }))
 }
 
 export default textures
