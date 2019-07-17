3e6073a508c6698b9b4fc5e3d5b137c90aa80c9f
CraftyDH <23249291+CraftyDH@users.noreply.github.com>
2019-07-17 14:54:18 +1000

    Revert "Make loader based on the amount loaded"
    This reverts commit 4cb549604de4434a3ae6babec7579987bcfae203.

--- src/overlays/loading/index.js
+++ src/overlays/loading/index.js
@@ -10,15 +10,12 @@ export default class LoadingOverlay {
     this.reason = this.el.querySelector('#reason').innerHTML
     style.textContent = loaderStyle
 
-    this.loader = this.el.querySelector('#loader')
-
     this.el.prepend(style)
 
     document.body.appendChild(this.el)
     if (percent) {
       this.percent = this.el.querySelector('#percent')
       this.percent.style.visibility = 'visible'
-      this.loader.setAttribute('value', 0)
     }
   }
 
@@ -32,6 +29,5 @@ export default class LoadingOverlay {
 
   set value(value) {
     this.percent.innerHTML = `${value.toFixed(2)}%`
-    this.loader.setAttribute('value', value)
   }
 }
