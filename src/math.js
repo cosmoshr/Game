393c1f0a74ac5bc2d4a7297fcdbd87923cd1d4a4
trickypr <trickypr@users.noreply.github.com>
2019-07-22 12:06:41 +1000

    Planet info (#26)
    * Create top info
    
    Sidebar info has been postponed due to the fact that they are of a lower priority

--- src/math.js
+++ src/math.js
@@ -26,10 +26,10 @@ Math.circleIntersect = (solarSystemPos, solarSystem, solarSystems, solarSystemSp
 
 /**
  * Converts degrees and radius of a circle to x and y coordernates
- * @param {x} The x coordernate at the center of the circle
- * @param {y} The y coordernate at the center of the circle
- * @param {rad} The radius of the circle
- * @param {degrees} The degrees to the point you are trying to get
+ * @param {Number} centerX The x coordernate at the center of the circle
+ * @param {Number} centerY The y coordernate at the center of the circle
+ * @param {Number} rad The radius of the circle
+ * @param {Number} degrees The degrees to the point you are trying to get
  * @returns {Object} The x and y coordernates
  */
 Math.genPosOnCircle = (centerX, centerY, rad, degrees) => {
@@ -40,4 +40,34 @@ Math.genPosOnCircle = (centerX, centerY, rad, degrees) => {
     x,
     y
   }
+}
+
+/**
+ * Humanize a number e.g. 10k
+ * @param {Number} value The number you want to conbert
+ * @returns {String} Humanized number
+ * @author mischat
+ */
+Math.humanize = value => {
+  const mag = Math.magnitude(value)
+
+  if (mag <= 3) return value
+  if (mag > 3 && mag <= 6) return `${value.toString().substr(0, mag - 3)}K`
+  if (mag > 6 && mag <= 9) return `${value.toString().substr(0, mag - 6)}M`
+  if (mag > 9 && mag <= 12) return `${value.toString().substr(0, mag - 9)}B`
+  if (mag > 12 && mag <= 15) return `${value.toString().substr(0, mag - 12)}T`
+
+  return value
+}
+
+Math.magnitude = value => {
+  let v = value
+  let mag = 0
+
+  while (v > 1) {
+    mag++
+    v /= 10
+  }
+
+  return mag
 }
